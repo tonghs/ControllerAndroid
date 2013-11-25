57d86f0c3bd7f4951cedaf9bc8c930303e442f75
tonghs <tonghuashuai@126.com>
2013-11-25 23:20:43 +0800

    add sth

--- Controller/src/main/java/com/tonghs/MainActivity.java
+++ Controller/src/main/java/com/tonghs/MainActivity.java
@@ -1,8 +1,12 @@
 package com.tonghs;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Spinner dropdown = (Spinner)findViewById(R.id.modules);
+        String[] items = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, items);
+
+//        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
+//                R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdown.setAdapter(adapter);
+
     }
 
 
@@ -18,6 +32,17 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    public void onOnBtnClick(View v) {
+        int index = Integer.parseInt(v.getTag().toString());
+//        AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+//        dialog.setTitle(String.valueOf(index)).show();
+    }
+
+
+    public void onOffBtnClick(View v) {
+
     }
     
 }
