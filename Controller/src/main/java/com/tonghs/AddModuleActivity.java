b3e0e979d81acb838b7121286dc7de3d0f19fbb5
tonghuashuai <tonghuashuai@126.com>
2013-12-09 16:05:12 +0800

    addtion of module

--- Controller/src/main/java/com/tonghs/AddModuleActivity.java
+++ Controller/src/main/java/com/tonghs/AddModuleActivity.java
@@ -1,5 +1,6 @@
 package com.tonghs;
 
+import android.app.AlertDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.tonghs.manager.AreaMgr;
+import com.tonghs.manager.ModuleMgr;
+import com.tonghs.model.Area;
+import com.tonghs.model.Module;
+
+import java.util.List;
 
 public class AddModuleActivity extends ActionBarActivity {
 
@@ -18,6 +29,16 @@ public class AddModuleActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_module);
 
+        final Spinner spinnerArea = (Spinner)findViewById(R.id.areas);
+
+        List<Area> listArea = new AreaMgr(this).getAreas();
+
+        if(listArea != null && listArea.size() > 0){
+            ArrayAdapter<Area> adapter = new ArrayAdapter<Area>(this,
+                    android.R.layout.simple_spinner_item, listArea);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinnerArea.setAdapter(adapter);
+        }
 
     }
 
@@ -32,13 +53,54 @@ public class AddModuleActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-//        switch (item.getItemId()) {
-//            case R.id.action_settings:
-//                return true;
-//        }
         return super.onOptionsItemSelected(item);
+    }
+    public void alert(String msg){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(AddModuleActivity.this);
+        dialog.setTitle(msg).show();
+    }
+
+    public void btnAddModuleClick(View v){
+        Spinner spinnerArea = (Spinner)findViewById(R.id.areas);
+        EditText txtName = (EditText)findViewById(R.id.txt_module_name);
+        EditText txtIP = (EditText)findViewById(R.id.txt_module_ip);
+        EditText txtPort = (EditText)findViewById(R.id.txt_module_port);
+        EditText txtFun1 = (EditText)findViewById(R.id.txt_module_fun1);
+        EditText txtFun2 = (EditText)findViewById(R.id.txt_module_fun2);
+        EditText txtFun3 = (EditText)findViewById(R.id.txt_module_fun3);
+        EditText txtFun4 = (EditText)findViewById(R.id.txt_module_fun4);
+        EditText txtFun5 = (EditText)findViewById(R.id.txt_module_fun5);
+        EditText txtFun6 = (EditText)findViewById(R.id.txt_module_fun6);
+
+        int areaId = ((Area)spinnerArea.getSelectedItem()).getId();
+        String name = txtName.getText().toString();
+        String ip = txtIP.getText().toString();
+        String port = txtPort.getText().toString();
+        String fun1 = txtFun1.getText().toString();
+        String fun2 = txtFun2.getText().toString();
+        String fun3 = txtFun3.getText().toString();
+        String fun4 = txtFun4.getText().toString();
+        String fun5 = txtFun5.getText().toString();
+        String fun6 = txtFun6.getText().toString();
+
+        if (name.equals("") || ip.equals("") || port.equals("")){
+            alert("必填项不可为空");
+        } else {
+            Module module = new Module();
+            module.setName(name);
+            module.setIp(ip);
+            module.setPort(Integer.parseInt(port));
+            module.setFun1(fun1.equals("") ? txtFun1.getHint().toString() : fun1);
+            module.setFun2(fun2.equals("") ? txtFun2.getHint().toString() : fun2);
+            module.setFun3(fun3.equals("") ? txtFun3.getHint().toString() : fun3);
+            module.setFun4(fun4.equals("") ? txtFun4.getHint().toString() : fun4);
+            module.setFun5(fun5.equals("") ? txtFun5.getHint().toString() : fun5);
+            module.setFun6(fun6.equals("") ? txtFun6.getHint().toString() : fun6);
+            module.setAreaId(areaId);
+
+            ModuleMgr mm = new ModuleMgr(this);
+            mm.add(module);
+            alert("添加成功");
+        }
     }
 }
